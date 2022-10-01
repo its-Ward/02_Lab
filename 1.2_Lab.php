816907c4feab55f3a1b6cf950261a13593a5b1c9
its-Ward <114559368+its-Ward@users.noreply.github.com>
2022-10-01 16:24:05 +0300

    Add files via upload

---
+++ 1.2_Lab.php
@@ -0,0 +1,18 @@
+<?php
+
+$n = 1;
+$s = "1";
+$n1 = $n.$s; 
+echo $n2 = $n1 ."10 little penguins";
+echo $n3 = "hello";
+echo  $n4 = "world";
+$n5 = $n3.$n4;
+$n6 = $n3.$n4;
+
+
+?>
+Greetings Ms. Layla,
+I would like to inform you that I have discovered the issue within the LAB assignment
+ which is a typo that includes the 
+ symbol (+) it only works on java not php because php uses a (.)
+  and there is no print statement (echo) that's why I had an issue with code.
